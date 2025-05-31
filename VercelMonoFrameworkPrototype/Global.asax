9fd25912a720ccc1ea2b97824b94f063055cf677
arestinum <patrikas.lyddon@gmail.com>
2025-05-31 12:52:16 +0100

    Create a dedicated Application manager class, taking in the application and storing itself in the ApplicationState. Instance made in Global.asax in Application_Start procedure

--- VercelMonoFrameworkPrototype/Global.asax
+++ VercelMonoFrameworkPrototype/Global.asax
@@ -1,5 +1,11 @@
 <%@ Application Language="C#" %>
 
+<%@ Import Namespace="System.Web" %>
+<%@ Import Namespace="VercelMonoFrameworkPrototypeLibrary" %>
+
 <script runat="server">
-    
+    void Application_Start(object sender, EventArgs e)
+    {
+        var vercelApp = new VercelMonoFrameworkApplication((HttpApplication)sender);
+    }
 </script>
--- VercelMonoFrameworkPrototypeLibrary/VercelFrameworkApplication.cs
+++ VercelMonoFrameworkPrototypeLibrary/VercelFrameworkApplication.cs
@@ -1,1 +1,16 @@
+using System.Web;
+
 namespace VercelMonoFrameworkPrototypeLibrary;
+
+public class VercelMonoFrameworkApplication
+{
+    public VercelMonoFrameworkApplication(HttpApplication application)
+    {
+        application.Application["VercelFramework"] = this;
+    }
+
+    public string GreetUser(string name)
+    {
+        return $"Hello {name}!";
+    }
+}
